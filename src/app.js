28e8f1217b9640ffc3de2544f5e832b9c0ebd291
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2024-04-08 15:43:53 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,27 +1,15 @@
-import api from './api/index';
+import api from './api/index.js';
+import express from 'express';
 const app = express();
 
 app.use('/public', express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use('/api/v1', api);
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to my REST API!');
 });
 
-app.get('/api/v1/cat', (req, res) => {
-  const cat = {
-    cat_id: 1,
-    name: 'Viski',
-    birthdate: '2001-02-01',
-    weight: 12,
-    owner: 3,
-    image: 'https://loremflickr.com/320/240/cat',
-  };
-  res.json(cat);
-});
-
-app.use('/public', express.static('public'));
-
+app.use('/api/v1', api);
 export default app;
